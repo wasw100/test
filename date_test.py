4132d169e70458451ac7071c7863fc059af24281
wasw100 <wasw100@gmail.com>
2014-05-20 22:47:02 +0800

    add method get_earliest_time

--- date_test.py
+++ date_test.py
@@ -5,14 +5,20 @@ import datetime
 import pytz
 import calendar
 
-def get_today_by_timezone(tz):
-    now = time.time()
-    utc_now = pytz.utc.localize(datetime.datetime.utcfromtimestamp(now))
+def get_today_by_timezone(timestamp=time.time(), tz=pytz.timezone('Etc/GMT0')):
+    """timezone day start time"""
+    utc_now = pytz.utc.localize(datetime.datetime.utcfromtimestamp(timestamp))
     tz_now = tz.normalize(utc_now)
-    t = int(now - tz_now.hour * 3600 - tz_now.minute * 60 - tz_now.second)
+    t = int(timestamp - tz_now.hour * 3600 - tz_now.minute * 60 - tz_now.second)
     return t
 
+def get_earliest_time(year, month, day):
+    """the earliest time of one day in the word"""
+    d = datetime.datetime(year, month, day)
+    return calendar.timegm(d.timetuple()) - 12*3600
+
 def get_day_start_time(timestamp, tz=pytz.timezone('Etc/GMT+12')):
+    """the earliest time of one day in the word"""
     d1 = datetime.datetime.fromtimestamp(timestamp, tz=tz)
     print d1
     d2 = datetime.datetime(d1.year, d1.month, d1.day, tzinfo=tz)
@@ -21,7 +27,8 @@ def get_day_start_time(timestamp, tz=pytz.timezone('Etc/GMT+12')):
 
 
 if __name__ == '__main__':
-    tz = pytz.timezone('Etc/GMT+12')
-    t1 = get_day_start_time(time.time(), tz)
-    t2 = get_today_by_timezone(tz)
+    tz = pytz.timezone('Etc/GMT-12')
+    # t1 = get_day_start_time(time.time(), tz)
+    t1 = get_earliest_time(2014, 5, 20)
+    t2 = get_today_by_timezone(time.time(), tz)
     print t1, t2, (t2-t1)/3600
