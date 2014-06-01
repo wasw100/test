71dd9779c268014765a25b5c7c2f9f546c124c4e
wasw100 <wasw100@gmail.com>
2014-06-01 23:03:23 +0800

    re find_all

--- re_test.py
+++ re_test.py
@@ -3,12 +3,15 @@
 import re
 
 def test():
-    s = 'a123b'
+    s = 'a123b456c'
     m = re.search(r'\d+', s)
     if m:
         print m.group()
     else:
         print 'no match'
+
+    ds = re.findall(r'\d+', s)
+    print ds
 
 if __name__ == '__main__':
     test()
