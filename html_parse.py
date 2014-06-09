f9338c09f1f9e936f74e81ba0e3ee9932bc8a4b3
wasw100 <wasw100@gmail.com>
2014-06-09 15:39:11 +0800

    add html_parse

---
+++ html_parse.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+网页内容解析
+1.正则
+2.pyquery
+3.xpath
+"""
+
+import lxml.html
+
+html = """
+<!doctype html>
+<html><body>
+<div id="content">
+<ul class="nav">
+<li><a href="http://test.com/post/1" target="_blank">1</a></li>
+<li><a href="http://test.com/post/2" target="_blank">2</a></li>
+<li><a href="http://test.com/post/3" target="_blank">3</a></li>
+<li><a href="http://test.com/post/4" target="_blank">4</a></li>
+<li><a href="http://test.com/post/5" target="_blank">5</a></li>
+<ul>
+<div>
+</body><html>
+"""
+
+
+def xpath_test():
+    """parse html use xpath"""
+    dom = lxml.html.fromstring(html)
+    hrefs = dom.xpath('//*[@id="content"]/ul/li[*]/a')
+    print len(hrefs)
+    for href in hrefs:
+        print href.attrib
+        print href.text
+
+
+if __name__ == '__main__':
+    xpath_test()
+
+
+
