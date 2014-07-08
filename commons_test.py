43556c7af90f10db5f62e18e7e1febd1b8392682
wasw100 <wasw100@gmail.com>
2014-07-08 10:20:10 +0800

    os.path test

--- commons_test.py
+++ commons_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import  os.path
 
 def set_test():
     s = set('abc')
@@ -23,6 +24,21 @@ def check_str():
     print isinstance(s, str)
 
 
+def path_join_test():
+    """
+    使用os.path.join 第二个参数一般情况下是一个相对路径
+    :return:
+    """
+    print '__file__:', __file__
+    cur_path = os.path.dirname(__file__)
+    print 'cur_path', cur_path
+    join_path = os.path.join('/Users/w3/Documents/github/', '/ddd')
+    print join_path
+    join_path2 = os.path.join('/Users/w3/Documents/github/', 'ddd')
+    print join_path2
+
+
 if __name__ == '__main__':
     # set_test()
-    check_str()
+    # check_str()
+    path_join_test()
